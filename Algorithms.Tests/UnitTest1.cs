3b713110fe0afcd04a81f20c1aa05be349978460
LT <support@teran.tech>
2020-12-21 07:47:53 -0700

    Code formatting.

--- Algorithms.Tests/UnitTest1.cs
+++ Algorithms.Tests/UnitTest1.cs
@@ -3,12 +3,12 @@ using Xunit;
 
 namespace Algorithms.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+   public class UnitTest1
+   {
+      [Fact]
+      public void Test1()
+      {
 
-        }
-    }
+      }
+   }
 }
--- DataStructures.Tests/UnitTest1.cs
+++ DataStructures.Tests/UnitTest1.cs
@@ -3,12 +3,12 @@ using Xunit;
 
 namespace DataStructures.Tests
 {
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+   public class UnitTest1
+   {
+      [Fact]
+      public void Test1()
+      {
 
-        }
-    }
+      }
+   }
 }
