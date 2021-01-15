0269debb1c249a19c77b6e4fa830c0ec55975a89
LT <support@teran.tech>
2021-01-14 20:37:53 -0700

    DataStructures.TreeTests    - Added testing framework for Trees

---
+++ DataStructures.Tests/TreeTests/BinaryTreeTests.cs
@@ -0,0 +1,15 @@
+using Trees;
+using Xunit;
+using System;
+
+namespace DataStructures.TreeTests
+{
+   public class BinaryTreeTests 
+   {
+      [Fact]
+      public void SampleTest()
+      {
+         
+      }
+   }
+}
