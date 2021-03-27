e850d61c957fb008dcbe8f1b9798d98ba137731d
LT <support@teran.tech>
2021-03-26 21:42:08 -0700

    Added hashing Algorithms folder.

---
+++ Algorithms/Hashing/Hasher.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Algorithms.Hashing
+{
+   public interface Hasher
+   {
+      static string Hash(string input);
+   }
+}
---
+++ Algorithms/Hashing/SHA256.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Algorithms.Hashing
+{
+   public class SHA256 : Hasher
+   {
+      public string Hash(string input)
+      {
+         return string.Empty;
+      }
+   }
+}
