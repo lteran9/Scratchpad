0c6f8c15ff2040c88087a140560604e1ff989000
LT <support@teran.tech>
2021-12-12 10:41:36 -0700

    Linked Lists - Added common interview question definitions.

--- DataStructures.Tests/LinkedListTests/InterviewQuestions.cs
+++ DataStructures.Tests/LinkedListTests/InterviewQuestions.cs
@@ -4,6 +4,7 @@ using Xunit;
 
 namespace DataStructures.LinkedListTests
 {
+   /// Common Interview Questions for Linked Lists
    public class InterviewQuestions
    {
       /// <summary>
@@ -32,10 +33,73 @@ namespace DataStructures.LinkedListTests
       }
 
       /// <summary> 
-      /// Return the Kth to last element in the linked list.
+      /// Return the Kth to Last:
+      /// Implement an algorithm to find the kth to last element of a singly linked list.
       /// </summary>
       [Fact]
       public void ReturnKthToLast()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Delete Middle Node:
+      /// Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+      /// </summary>
+      [Fact]
+      public void DeleteMiddleNode()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Partition:
+      /// Write code to partition a linked list around a value x, such that all nodes less than x come before all nodes grater than or equal to x. If x is contained within the list, the values of x only need to be after the elements less than x (see below).
+      /// </summary>
+      [Fact]
+      public void Partition()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Sum Lists:
+      /// You have two numbers represented by a linked list, where each node contains a single digit. The digits are stored in reverse order, such that the 1's digit is at the head of the list. Write a function that adds the two numbers and returns the sum as a linked list.
+      /// </summary>
+      [Fact]
+      public void SumLists()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Palindrome:
+      /// Implement a function to check if a linked list is a palindrome.
+      /// </summary>
+      [Fact]
+      public void Palindrome()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Intersection:
+      /// Given two (singly) linked lists, determine if the two lists intersect. Return th eintersecting node. Note that the intersection is defined based on reference, not value. That is, if the kth node of the first linked list is the exact same node (by reference) as the jth node of the second linked list, then they are intersecting.
+      /// </summary>
+      [Fact]
+      public void Intersection()
+      {
+         //
+      }
+
+      /// <summary>
+      /// Loop Detection:
+      /// Given a circular linked list, implement an algorithm that returns the node at the beginning of the loop. 
+      ///
+      /// Circular linked list: A (corrupt) linked list in which a node's next pointer points to an earlier node, so as to make a loop in the linked list.
+      /// </summary>
+      [Fact]
+      public void LoopDetection()
       {
          //
       }
