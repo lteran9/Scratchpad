d1f59fa911a626c14f2bc38143be960c4b16cf26
LT <support@teran.tech>
2022-08-18 17:21:54 -0700

    Chapter 1.1 - IsUnique Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?

--- DataStructures/Strings/String.cs
+++ DataStructures/Strings/String.cs
@@ -5,7 +5,7 @@ using System.Linq;
 namespace DataStructures.Strings
 {
    /// <summary>
-   /// Be careful with this class because it hides the .NET framework String.cs implementation.
+   /// Be aware this class hides the .NET framework String implementation. Only reference this object through it's namespace.
    /// </summary>
    public class String
    {
@@ -53,6 +53,41 @@ namespace DataStructures.Strings
                // Group characters, select first, and compare length of original string
                return value.ToCharArray().GroupBy(x => x).Select(x => x.First()).Count() == value.Length;
             }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
+         }
+
+         return false;
+      }
+
+      public static bool HasAllUniqueCharacters_ASCII(string value)
+      {
+         try
+         {
+            // Validate passed in value is not empty
+            if (string.IsNullOrEmpty(value) == false)
+            {
+               // There are only 128 characters in the ASCII alphabet
+               var seen = new bool[128];
+               for (int i = 0; i < value.Length; i++)
+               {
+                  // Each index in the ASCII alphabet corresponds to a character
+                  if (!seen[value[i]])
+                  {
+                     seen[value[i]] = true;
+                  }
+                  else
+                  {
+                     // We have seen this ASCII character so return false
+                     return false;
+                  }
+               }
+            }
+
+            // Did not see duplicate ASCII character
+            return true;
          }
          catch (Exception ex)
          {
