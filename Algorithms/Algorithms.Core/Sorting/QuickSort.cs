4d935255be692ba26d2b4677ce3c4869625840bc
LT <support@teran.tech>
2024-05-12 21:13:39 -0700

    Added QuickSort class definition, still need to work on implementation.

---
+++ Algorithms/Algorithms.Core/Sorting/QuickSort.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Core.Sorting
+{
+   public static class QuickSort
+   {
+      public static List<T> Sort<T>(List<T> collection, SortOrder order = SortOrder.ASC) where T : IComparable<T>
+      {
+         return collection;
+      }
+   }
+}
